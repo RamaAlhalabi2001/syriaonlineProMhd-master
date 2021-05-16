5e3ff6e8bd26427b8d70007dfb04cbb7ea7eb7e7
Rama Alhalbi <rama.ramroom.exo@gmail.com>
2021-05-16 17:15:35 +0300

    just id stayed

--- lib/utils/allUrl.dart
+++ lib/utils/allUrl.dart
@@ -3,7 +3,7 @@ String comment = "https://gp-tcc.000webhostapp.com/public/api/comment";
 
 String account = "https://gp-tcc.000webhostapp.com/public/api/account";
 
-String services = "https://gp-tcc.000webhostapp.com/public/api/services/";
+String services = "https://gp-tcc.000webhostapp.com/public/api/services/62";
 String servicescatogary =
     "https://gp-tcc.000webhostapp.com/public/api/servicescatogary";
 
